b956a229a93fdbd8fdeff7c03356dad3015ea130
astoisavljevic <astoisavljevic@jatheon.com>
2025-02-15 14:41:46 +0100

    Using dot env for loading environment properties

--- src/main/java/com/jatheon/ergo/ai/assistant/app/Application.java
+++ src/main/java/com/jatheon/ergo/ai/assistant/app/Application.java
@@ -1,10 +1,12 @@
 package com.jatheon.ergo.ai.assistant.app;
 
 import com.jatheon.ergo.ai.assistant.config.ApplicationConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+@Slf4j
 @Import({
         ApplicationConfig.class
 })
