05d24249508acdaf53d2ddb78cb485716ae821df
astoisavljevic <astoisavljevic@jatheon.com>
2025-02-20 23:43:25 +0100

    implemented summarization, still need to do it in UI wip#2

--- src/main/java/com/jatheon/ergo/ai/assistant/service/util/ClearDataUtil.java
+++ src/main/java/com/jatheon/ergo/ai/assistant/service/util/ClearDataUtil.java
@@ -10,16 +10,16 @@ public class ClearDataUtil {
     }
 
     public static String clearData(String data) {
-        log.debug("result > 0: {}", data);
+        log.trace("result > 0: {}", data);
         String result = data;
         if (data.startsWith("\"")) {
             result = data.substring(1, data.length() - 1);
         }
-        log.debug("result > 1: {}", result);
+        log.trace("result > 1: {}", result);
         if (result.endsWith("\"")) {
             result = result.substring(0, data.length() - 1);
         }
-        log.debug("result > 2: {}", result);
+        log.trace("result > 2: {}", result);
         return result;
     }
 
